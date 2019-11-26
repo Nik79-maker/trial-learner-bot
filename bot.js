e4c237cb82fb5992bbc65870614e28bf235705ee
Nik79-maker <58207307+Nik79-maker@users.noreply.github.com>
2019-11-26 14:19:09 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,27 @@
 var Discord = require('discord.js');
-var botName = require('./package.json');
+var botName = require('./bot. Js');
 var bot = new Discord.Client();
 
-bot.on('message', message =>{
+bot.on('message', message =/{
 
     // Variables
     var sender = message.author; //The person who sent the message
     var msg = message.content;
-    var prefix = ">" //The text before commands, you can put anything that you prefer
+    var prefix = "/" //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "381443488392413184" && message.channel.id === "381397780385628181"){
-        if(msg.startsWith('>', 0)){
-            if(msg === prefix + "bang" && message.channel.id === "381397780385628181"){
-                message.channel.send('BOOM!!!!!') // Sends a message to the channel, with the content
+    if(message.author.id != "648775225038733312" && message.channel.id === "648784575727665162"){
+        if(msg.startsWith('/', 0)){
+            if(msg === prefix + "hi" && message.channel.id === "648784575727665162"){
+                message.channel.send('hello!!!!!') // Sends a message to the channel, with the content
             }
             else{
-                message.channel.send("Hey, I only understand >bang right now. More will be added when my creator is free. :) ")
+                message.channel.send("Hey, I only understand /hello right now. More will be added when my creator is free. :) ")
             }
         }
         else{
             message.delete();
-            message.channel.send(sender + " this bot only accepts commands which starts with '>' dot.");
+            message.channel.send(sender + " this bot only accepts commands which starts with '/' dot.");
+  
         }
     }
 });
